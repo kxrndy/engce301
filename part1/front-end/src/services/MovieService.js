ebd658d993880ac7f7bce32b276f1bb6ce3ac90e
“S.Korndanai” <kxrndanai@gmail.com>
2025-01-11 01:24:01 +0700

    add lab2 and lab3

---
+++ part1/front-end/src/services/MovieService.js
@@ -0,0 +1,40 @@
+export async function searchMovie(search_data) {
+
+    try {
+
+        console.log('search_data: ' + search_data);
+
+        const response = await fetch('http://192.168.44.128:3001/api/movie/search?search_text=' + search_data);
+
+        return await response.json(); //***
+
+        // console.log('Search Movie response: ' + await response);
+
+    } catch (error) {
+        return [];
+    }
+
+}
+
+export async function getAllMovies() {
+
+    try{
+        //const response = await fetch('/api/users');
+         const response = await fetch('http://192.168.44.128:3001/api/movie/all');
+        //const response = await fetch('/api/movie/all');
+        return await response.json();
+    }catch(error) {
+        return [];
+    }
+    
+}
+
+//---- แก้ไข Code ที่ services/MovieService.js ----
+export async function createMovie(data) {
+    const response = await fetch(`http://192.168.44.128:3001/api/movie/insert`, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+      })
+    return await response.json();
+}
